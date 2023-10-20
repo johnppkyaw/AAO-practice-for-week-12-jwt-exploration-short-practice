98ccc7432f5df6f6957fd822b35d9f82e75c6332
John <jjpyae@gmail.com>
2023-10-20 12:22:33 -0700

    practice complete

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ require('dotenv').config();
 
 // Import package
 
-// Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
 // 1. Sign (create) a JWT containing your email address
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
-// Your code here
+const payload1 = {
+  email: "johnny@gmail.com"
+}
+const secret = process.env.SECRET_KEY;
+
+token = jwt.sign(payload1, secret, {
+  expiresIn: '1s'
+});
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -19,7 +26,7 @@ console.log('JWT:', token);
 
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
-// Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -28,7 +35,7 @@ console.log('Payload:', payload);
 
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
-// Your code here
+verifiedPayload = jwt.verify(token, secret);
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
@@ -38,7 +45,12 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify
 //    Then "catch" the error and log it to the console.
 
-// Your code here
+//JsonWebTokenError: invalid signature
+try {
+  verifiedPayload = jwt.verify(token, 'c98f1180b7b804cc718d1a3fbb767c2f9bb8d9bb2bceb3c38d360a057f1f52a3b06bcb36306fbf6aec4b7e8301ddcbec4cf4d0c55ba61b90bc2f31ece2b7e626')
+} catch (err) {
+  console.log(err);
+}
 
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
@@ -46,4 +58,11 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify (with proper secret)
 //    Then "catch" the error and log it to the console
 
-// Your code here
+//TokenExpiredError: jwt expired
+setTimeout(() => {
+  try {
+    verifiedPayload = jwt.verify(token, secret);
+  } catch (err) {
+    console.log(err);
+  }
+}, 2000);
